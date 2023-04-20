de51ed78309236f90fbb86d8c879a4068343465b
jhlee <wisdom_13_@naver.com>
2023-04-20 15:36:11 +0900

    ISR 렌더링

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 import { getProducts } from '@/service/products';
 
+export const revalidate = 3;
 
 export default async function ProductsPage() {
   const products = await getProducts();
