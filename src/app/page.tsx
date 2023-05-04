d003954ef8e1bdd01dad7a061ddc3576a735ade4
지혜 <77341912+wisdom-13@users.noreply.github.com>
2023-05-04 17:29:07 +0900

    main 브랜치 (#1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Counter from '@/components/Counter'
 export default function Home() {
   return (
     <>
-      <h1>home v3</h1>
+      <h1>home v3 - main</h1>
       <Counter />
       <Image src="https://images.unsplash.com/photo-1441986300917-64674bd600d8" alt='shop' width={400} height={400} />
     </>
